3a46fbf4406ea1a671853daa3fefd827b1275ce7
Mark <lisantimark@gmail.com>
2022-03-27 03:09:51 -0600

    first commit

---
+++ Assets/Materials/KinectDepthBasic.shader
@@ -0,0 +1,96 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// SPECIAL THANKS TO https://github.com/izmhr/KinectV2DepthPoints for the   //
+// CODE [now modified] AND INSPIRATION                                      //
+//////////////////////////////////////////////////////////////////////////////
+
+Shader "Custom/KinectDepthBasic"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_Displacement("Displacement", Range(0, 0.1)) = 0.003
+		_Color("Particle Color", Color) = (1,0,1,1)
+		
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 5
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float4 col : COLOR;
+			};
+
+			sampler2D _MainTex;
+			float _Displacement;
+			fixed4 _Color;
+
+			float4 _MainTex_ST;
+
+
+			v2f vert(appdata v)
+			{
+				// depth information based on color info of texture
+				float4 col = tex2Dlod(_MainTex, float4(v.uv, 1, 0));
+	
+				float d = col.x * 1500 * _Displacement;
+
+				// expand the points to space with depth coordinate data
+				v.vertex.x = v.vertex.x * d / 3.656;
+				v.vertex.y = v.vertex.y * d / 3.656;
+				v.vertex.z = d;		
+			
+				// transform the mesh to pass to vertex shader
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv = v.uv;
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+
+				fixed4 col;
+
+				// texture color
+				//fixed4 col2 = tex2D(_MainTex, i.uv);
+				
+				// predefined color
+				//col = _Color;
+				
+				// check posiiton on point
+				if (i.vertex.z > 0.0006) 
+                		{
+					
+					col = _Color;
+				}
+				// discard if not close enough
+				else 
+                		{
+					discard;	
+				}
+				
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Scripts/PlaneGenerator.cs
@@ -0,0 +1,79 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// SPECIAL THANKS TO https://github.com/izmhr/KinectV2DepthPoints for the   //
+// CODE [now modified] AND INSPIRATION                                      //
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneGenerator : MonoBehaviour
+{
+	// width
+	[SerializeField]
+	private int w = 1024/2;
+
+	// height
+	[SerializeField]
+	private int h = 844/2;
+
+	[SerializeField]
+	private float pitch = .01f;
+
+	void Awake()
+	{
+		GetComponent<MeshFilter>().mesh = MeshGenerate();
+	}
+
+	Mesh GenerateMesh(int w, int h, float pitch)
+	{
+		Mesh mesh = new Mesh();
+		mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+		Vector3[] vertices = new Vector3[w * h];
+		for (var y = 0; y < h; y++)
+		{
+			for (var x = 0; x < w; x++)
+			{
+				vertices[y * w + x] = new Vector3((float)(x - w / 2) * pitch, (float)(y - h / 2) * pitch, 0f);
+			}
+		}
+		mesh.vertices = vertices;
+
+		Vector2[] uv = new Vector2[w * h];
+		for (var y = 0; y < h; y++)
+		{
+			for (var x = 0; x < w; x++)
+			{
+				uv[y * w + x] = new Vector2(((float)x + 0.5f) / (float)w, ((float)y + 0.5f) / (float)h);
+			}
+		}
+		mesh.uv = uv;
+
+		int[] triangles = new int[(w - 1) * (h - 1) * 2 * 3];
+		for (var y = 0; y < h - 1; y++)
+		{
+			for (var x = 0; x < w - 1; x++)
+			{
+				triangles[(y * (w - 1) + x) * 2 * 3 + 0] = (y * w + x);
+				triangles[(y * (w - 1) + x) * 2 * 3 + 1] = (y * w + x + 1);
+				triangles[(y * (w - 1) + x) * 2 * 3 + 2] = ((y + 1) * w + x + 1);
+				triangles[(y * (w - 1) + x) * 2 * 3 + 3] = (y * w + x);
+				triangles[(y * (w - 1) + x) * 2 * 3 + 4] = ((y + 1) * w + x + 1);
+				triangles[(y * (w - 1) + x) * 2 * 3 + 5] = ((y + 1) * w + x);
+			}
+		}
+		mesh.triangles = triangles;
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+		return mesh;
+	}
+
+	Mesh MeshGenerate()
+	{
+		return GenerateMesh(w, h, pitch);
+	}
+}
