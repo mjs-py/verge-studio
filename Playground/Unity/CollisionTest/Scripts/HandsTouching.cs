7848b93f2b3dc4b7630b526005afa1ca1958de3b
Mark <lisantimark@gmail.com>
2022-03-27 22:11:41 -0600

    skeleton prefab

---
+++ Playground/Unity/CollisionTest/Scripts/HandsTouching.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsTouching : MonoBehaviour
+{
+    private int count = 0;
+    public float speed = 1.0F;
+
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var main = ps.main;
+        Debug.Log(speed);
+        main.simulationSpeed = speed;
+    }
+
+    // Collision
+    void OnTriggerEnter(UnityEngine.Collider collision) 
+    {
+        // check if hands is touching object
+        // count the number of hands touching if 
+        if (collision.gameObject.name == "HandLeft" || collision.gameObject.name == "HandRight") 
+        {
+            if (count < 2) 
+            {
+                count += 1; 
+            }
+            switch (count)
+            {
+                case 1:
+                    Debug.Log("One hand touching");
+                    // do something
+                    break;
+                case 2: 
+                    Debug.Log("Two hands touching");
+                    // change particle speed rate
+                    speed = 0.1F;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit(UnityEngine.Collider collision)
+    {
+        if (collision.gameObject.name == "HandLeft" || collision.gameObject.name == "HandRight") 
+        {
+            if (count > 0) 
+            {
+                count -= 1; 
+            }
+            speed = 1.0F;
+            Debug.Log(collision.gameObject.name + "Hand leaving object");
+        }
+    }
+}
